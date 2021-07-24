ff27d02b13440b87598a37d9efb91b5ffb73099b
john-corcoran <24858700+john-corcoran@users.noreply.github.com>
2021-07-24 13:03:05 +0100

    wipedrive.sh added

---
+++ wipe_drive_and_verify/wipedrive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Wipe a drive using dd, then read the drive back and confirm only zeroes are present
+# Usage: sudo ./wipedrive.sh [disk name as listed in 'sudo fdisk -l', e.g. 'sda']
+# Usage example: sudo ./wipedrive.sh sda
+# A successful output from the pv command (i.e. disk read to confirm all zeroes) should look
+# something like:
+# 0000000 000000 000000 000000 000000 000000 000000 000000 000000
+# *
+# [total size of drive]
+dd if=/dev/zero of=/dev/$1 bs=1M status=progress
+pv /dev/$1 | od | head
