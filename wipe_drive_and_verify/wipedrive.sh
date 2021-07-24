1f439974123837e134860056df5eae6e9099f3b1
john-corcoran <24858700+john-corcoran@users.noreply.github.com>
2021-07-24 14:05:10 +0100

    Usage example changed to less dangerous 'sdz'

--- wipe_drive_and_verify/wipedrive.sh
+++ wipe_drive_and_verify/wipedrive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Wipe a drive using dd, then read the drive back and confirm only zeroes are present
-# Usage: sudo ./wipedrive.sh [disk name as listed in 'sudo fdisk -l', e.g. 'sda']
-# Usage example: sudo ./wipedrive.sh sda
+# Usage: sudo ./wipedrive.sh [disk name as listed in 'sudo fdisk -l', e.g. 'sdz']
+# Usage example: sudo ./wipedrive.sh sdz
 # A successful output from the pv command (i.e. disk read to confirm all zeroes) should look
 # something like:
 # 0000000 000000 000000 000000 000000 000000 000000 000000 000000
